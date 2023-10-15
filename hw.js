0e936ad894cef43a7993d3e5e21ff6fdafbd2c0b
affaf04 <affaftaouti@gmail.com>
2023-10-15 01:19:15 +0100

    The last change

--- hw.js
+++ hw.js
@@ -4,7 +4,7 @@
 // - If students get 60 or above: console log ehhhh
 // - If students get 55 or above: console log Not to good
 // // - Any grade lower than 55 is Bad Grade But You'll Get Em Next Time
-/*function grading(grade) {
+function grading(grade) {
     if (grade >= 80){
         console.log ('You did a good job')
     } else if (grade>=70){
@@ -16,33 +16,35 @@
     } else {
         console.log ("Bad grade you'll Get Em Next Time")
     }
-}*/
+}
+grading(100);
 
 
 // 2.   Write a function that prints out multiples of 10 up to a given input (argument)
 
-/*function multiplesOfTen(){
-    for (let i=10; i<=100; i += 10 )
-    console.log(i)
-}*/
+function multiplesOfTen(){
+    for (let i=10; i<=100; i += 10)
+    console.log(i);
+}
+multiplesOfTen(); 
 
 
 // 3.   Write a function that takes bill amount and item price and says how many quarters they'd get in return
 
-/*function changeInQuarters(billAmount, price){
+function changeInQuarters(billAmount, price){
     let change = billAmount- price;
     let quarters = change / 0.25
     console.log(quarters)
 
 }
-changeInQuarters(10,4)*/
+changeInQuarters(10,4);
 
 
 
 //4.    Write a function that prints out how many bills you would recieve in change after paying a certain amount.  Return in as large denominations as possible. Parameters should be amount paind and amount cost
 
 function change(amountPaid, amountCost) {
-    let denominations = [100, 50, 10, 20, 5, 2, 1];
+    let denominations = [100, 50, 20, 10, 5, 2, 1];
     let change = amountPaid - amountCost;
     console.log(change);
     denominations.forEach(function (denomination) {  
@@ -56,8 +58,7 @@ function change(amountPaid, amountCost) {
         }
     });
 }
-
-change(75, 15);
+change(100, 37);
 
 
 
@@ -67,7 +68,7 @@ change(75, 15);
 
 // 5.   Write a program that uses console.log to print all the numbers from 1 to 100 with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 
-/*let x = 1;
+let x = 1;
 while (x <= 100) {
     if (x % 3 === 0 && x % 5 === 0) {
         console.log("FizzBuzz");
@@ -79,5 +80,5 @@ while (x <= 100) {
         console.log(x);
     }
     x++;
-}*/
+}
    
